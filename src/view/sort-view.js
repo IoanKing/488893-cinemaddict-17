7bd6663d3f50acc7ded4e46b58a672f5583a9a11
Иван Суслов <ioankingisepp@mail.ru>
2022-06-12 13:46:25 +0300

    Fix sort view.

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -1,9 +1,10 @@
 import AbstractView from '../framework/view/abstract-view.js';
+import {SortType} from '../const.js';
 
-const createFilterTemplate = () => `<ul class="sort">
-<li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-<li><a href="#" class="sort__button">Sort by date</a></li>
-<li><a href="#" class="sort__button">Sort by rating</a></li>
+const createSortTemplate = () => `<ul class="sort">
+<li><a href="#" class="sort__button sort__button--active" data-sort-view="${SortType.DEFAULT}">Sort by default</a></li>
+<li><a href="#" class="sort__button" data-sort-view="${SortType.BY_DATE}">Sort by date</a></li>
+<li><a href="#" class="sort__button" data-sort-view="${SortType.BY_RATIO}">Sort by rating</a></li>
 </ul>`;
 
 export default class NewFilterView extends AbstractView {
@@ -12,6 +13,6 @@ export default class NewFilterView extends AbstractView {
   }
 
   get template() {
-    return createFilterTemplate();
+    return createSortTemplate();
   }
 }
