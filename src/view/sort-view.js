e2678c5546ab244c6535b1fd013ea41c374659d4
Иван Суслов <ioankingisepp@mail.ru>
2022-06-12 13:48:05 +0300

    Implements a subscription to change sorting

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -15,4 +15,18 @@ export default class NewFilterView extends AbstractView {
   get template() {
     return createSortTemplate();
   }
+
+  setSortTypeChangeHandler = (callback) => {
+    this._callback.sortTypeChange = callback;
+    this.element.addEventListener('click', this.#sortTypeChangeHandler);
+  };
+
+  #sortTypeChangeHandler = (evt) => {
+    if (evt.target.tagName !== 'A') {
+      return;
+    }
+
+    evt.preventDefault();
+    this._callback.sortTypeChange(evt.target.dataset.sortType);
+  };
 }
